b048bcaa9a47d5177c80ae05315a6b50b7838041
Zjorritte <JYBm5@hotmail.nl>
2015-02-02 17:04:39 +0100

    Je kan nu met je muis over één vakje gaan, (het is nu gelukt omdat ik <map> en </map> was vergeten). Maar het werkt alleen bij plaats 201. Plaatsen 202 en 203 werken niet, enig idee waarom?

--- polymap.php
+++ polymap.php
@@ -6,87 +6,99 @@
 			window.onload = function(){ 
 				var a = document.getElementById('201');
 				a.onmouseover = function() {
- 		 			document.getElementById('Adiv').style.display = 'block';
+ 		 			document.getElementById('201div').style.display = 'block';
 				}
 				a.onmouseout = function() {
- 			 		document.getElementById('Adiv').style.display = 'none';
+ 			 		document.getElementById('201div').style.display = 'none';
 				}
-				var b = document.getElementById('B');
+				a.onclick = function() {
+					document.getElementById('201boekdiv').style.display = 'block';
+				}
+				var b = document.getElementById('202');
 				b.onmouseover = function() {
- 		 			document.getElementById('Bdiv').style.display = 'block';
+ 		 			document.getElementById('202div').style.display = 'block';
 				}
 				b.onmouseout = function() {
- 			 		document.getElementById('Bdiv').style.display = 'none';
+ 			 		document.getElementById('202div').style.display = 'none';
 				}
 				b.onclick = function() {
-					document.getElementById('Bboekdiv').style.display = 'block';
+					document.getElementById('202boekdiv').style.display = 'block';
 				}
-				var c = document.getElementById('C');
+				var c = document.getElementById('203');
 				c.onmouseover = function() {
- 		 			document.getElementById('Cdiv').style.display = 'block';
+ 		 			document.getElementById('203div').style.display = 'block';
 				}
 				c.onmouseout = function() {
- 			 		document.getElementById('Cdiv').style.display = 'none';
+ 			 		document.getElementById('203div').style.display = 'none';
+				} 
+				c.onclick = function() {
+					document.getElementById('203boekdiv').style.display = 'block';
 				}
-
 			};
-			function validateForm()
-				{
+			function validateForm() {
 					var x=document.forms["contactform"]["voornaam"].value;
 					if (x==null || x=="")
   				{
   			alert("Geef alstublieft uw volledige naam op");
   			return false;
   				}
-  
-  var x=document.forms["contactform"]["achternaam"].value;
-if (x==null || x=="")
-  {
-  alert("Geef alstublieft uw achternaam op");
-  return false;
-  }
-  var x=document.forms["contactform"]["e-mail adres"].value;
-if (x==null || x=="")
-  {
-  alert("Geef alstublieft uw e-mail adres op");
-  return false;
-  }
-  var x=document.forms["contactform"]["vraag"].value;
-if (x==null || x=="")
-  {
-  alert("Why you send a mail, when you don't send any message to me?");
-  return false;
-  }
-}
+  					var x=document.forms["contactform"]["achternaam"].value;
+					if (x==null || x=="")
+  				{
+  			alert("Geef alstublieft uw achternaam op");
+  			return false;
+  				}
+  					var x=document.forms["contactform"]["e-mail adres"].value;
+					if (x==null || x=="")
+  				{
+  			alert("Geef alstublieft uw e-mail adres op");
+  			return false;
+  				}
+  					var x=document.forms["contactform"]["vraag"].value;
+					if (x==null || x=="")
+  				{
+  			alert("Why you send a mail, when you don't send any message to me?");
+  			return false;
+  				}
+			}
 
-		</script>
+	</script>
 
 	
 	</head>
 	<style>
-	#Adiv {
+	#201div {
 		position: fixed;
     	top: 50px;
     	left: 1100px;
     }
-	#Bdiv {
-    	position: fixed;
-    	top: 50px;
-    	left: 1000px;
-    }
-    #Bboekdiv {
+    #201boekdiv {
     	position: fixed;
     	top: 50px;
     	left: 1200px;
     }
-    #Cdiv {
+	#202div {
+    	position: fixed;
+    	top: 50px;
+    	left: 1000px;
+    }
+    #202boekdiv {
+    	position: fixed;
+    	top: 50px;
+    	left: 1200px;
+    }
+    #203div {
     	position: fixed;
     	top: 50px;
     	left: 1100px;
     }
-
+    #203boekdiv {
+    	position: fixed;
+    	top: 50px;
+    	left: 1200px;
+    }
     .image {
-    	width: 900px;
+    	width: 800px;
     	height: 600px;
     }
 	</style>
@@ -108,12 +120,11 @@ if (x==null || x=="")
     			echo '</pre>'; 
 			}
 			while($row = $result->fetch(PDO::FETCH_ASSOC)){
-				echo "<area shape='poly' id='$row[nummer]' class='mapping' coords='$row[coordinaten1];' />";
+				echo "<map name='me'><area shape='poly' id='$row[nummer]' class='mapping' coords='$row[coordinaten1];'/></map>";	
 			}
 			?>
-   		</div>
 
-   		<div id="Adiv" style="display: none">
+   		<div id="201div" style="display: none">
    		Plaats nr.: A <br>
    		Prijs (p.p per nacht): €genoeg <br>
    		Bezet op: (31-1 tot 10-2) <br>
@@ -123,7 +134,7 @@ if (x==null || x=="")
    		Bijzonderheden: ligt door plaats B <br>
    		</div>
    		
-   		<div id="Bdiv" style="display: none">
+   		<div id="202div" style="display: none">
    		Plaats nr.: B <br>
    		Prijs (p.p per nacht): €genoeg <br>
    		Bezet op: (31-1 tot 10-2) <br>
@@ -133,7 +144,7 @@ if (x==null || x=="")
    		Bijzonderheden: Zandloper figuur <br>
    		</div>
    		
-   		<div id="Cdiv" style="display: none">
+   		<div id="203div" style="display: none">
    		Plaats nr.: C <br>
    		Prijs (p.p per nacht): €genoeg <br>
    		Bezet op: (31-1 tot 10-2) <br>
@@ -143,7 +154,63 @@ if (x==null || x=="")
    		Bijzonderheden: geen <br>
    		</div>
    		
-   		<div id="Bboekdiv" style="display: none">
+   		<div id="201boekdiv" style="display: none">
+   		<form name="contactform" enctype="text/plain" onsubmit="return validateForm()" method="get">
+
+			Persoonlijke informatie:
+			<br>
+				Volledige naam: <input type="text" name="voornaam" placeholder="Achternaam - Voornaam">
+			<br>
+				Adres: <input type="text" name="postcode" placeholder="postcode"> 
+				<input type="text" name="huisnummer" placeholder="huisnummer">
+				<input type="text" name="land" placeholder="land">
+			<br>
+				Tel. nummer <input type="text" name="tel. nummer" placeholder="tel. nummer">
+			<br>
+				E-mail adres: <input type="text" name="e-mail adres" placeholder="e-mail adres">
+			<br>
+				Boekingsperiode:
+			<br>
+				Aantal personen: <input type="text" name="aantal personen" placeholder="aantal personen"
+			<br>
+				<input type="checkbox" name="Caravan" value="Caravan">Caravan
+				<br>
+				<input type="checkbox" name="Tent" value="Tent">Tent
+				<br>
+				<input type="checkbox" name="Camper" value="Camper">Camper
+			<br>
+
+		</form>
+   		</div>
+   		<div id="202boekdiv" style="display: none">
+   		<form name="contactform" enctype="text/plain" onsubmit="return validateForm()" method="get">
+
+			Persoonlijke informatie:
+			<br>
+				Volledige naam: <input type="text" name="voornaam" placeholder="Achternaam - Voornaam">
+			<br>
+				Adres: <input type="text" name="postcode" placeholder="postcode"> 
+				<input type="text" name="huisnummer" placeholder="huisnummer">
+				<input type="text" name="land" placeholder="land">
+			<br>
+				Tel. nummer <input type="text" name="tel. nummer" placeholder="tel. nummer">
+			<br>
+				E-mail adres: <input type="text" name="e-mail adres" placeholder="e-mail adres">
+			<br>
+				Boekingsperiode:
+			<br>
+				Aantal personen: <input type="text" name="aantal personen" placeholder="aantal personen"
+			<br>
+				<input type="checkbox" name="Caravan" value="Caravan">Caravan
+				<br>
+				<input type="checkbox" name="Tent" value="Tent">Tent
+				<br>
+				<input type="checkbox" name="Camper" value="Camper">Camper
+			<br>
+
+		</form>
+   		</div>
+   		 			<div id="203boekdiv" style="display: none">
    		<form name="contactform" enctype="text/plain" onsubmit="return validateForm()" method="get">
 
 			Persoonlijke informatie:
