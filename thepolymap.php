60f0990a017d1a856fc5addb4c5224d57e6db6d0
Zjorritte <JYBm5@hotmail.nl>
2015-02-15 13:34:30 +0100

    if (mailformat.match(!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/))) werkt om een of andere reden niet... ik ben bezig met mail validation, maar het werkt nog niet..

--- thepolymap.php
+++ thepolymap.php
@@ -7,7 +7,11 @@
 			 //echo "<script> document.getElementById('plaats').innerHTML=!!!; </script>"
 		?>
 		<script>
-		console.log(11); 
+		$(document).ready(function() {
+
+
+
+		});
 			window.onload = function(){ 
 				var a = document.getElementById('201');
 				a.onmouseover = function() {
@@ -40,10 +44,17 @@
 					document.getElementById('201boekdiv').style.display = 'block';
 				}
 			
-				};
+			};
 
 			function validateForm()
 			{
+				var mailformat=document.forms["boekform"]["email"].value;  
+				if (mailformat.match(!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/))) 
+				{  
+					alert("U heeft een onjuist e-mail adres opgegeven");  
+					return false;  
+				} 
+				
 				var allchecked=0;
 				if(document.getElementById('Caravan').checked){allchecked=1;}
 				if(document.getElementById('Tent').checked){allchecked=1;}
@@ -57,16 +68,21 @@
   					return false;
   				}
   				var x=document.forms["boekform"]["boekingsperiode"].value;
+
 				if (x==null || x=="")
   				{
-  					console.log(12);
-  					console.log(document.forms["boekform"]);
   					alert("Geef alstublieft uw boekingsperiode op");
   					return false;
   				}
-  			
+  				var x=document.forms["boekform"]["boekingsperiode2"].value;
+				if (x==null || x=="")
+  				{
+  					alert("Geef alstublieft uw boekingsperiode op");
+  					return false;
+  				}
+
+ 
   			}
-  			console.log(13);
 			
 	</script>
 
@@ -167,10 +183,12 @@
    		<form action= "info.php" method= "post" id="f1" name="boekform" enctype="text/plain" onsubmit="return validateForm()">
 			Boekingsinformatie:
 			<br>
-				Boekingsperiode: Vanaf:<input type="date" name="boekingsperiode" min="<?php echo date("Y-m-d") ?>">tot <input type="date" placeholder="dd-mm-jjjj" name="boekingsperiode" min="<?php echo date("Y-m-d") ?>">
+				Boekingsperiode: Vanaf:<input id=bp1 type="date" name="boekingsperiode" placeholder="jjjj-mm-dd" min="<?php echo date("Y-m-d") ?>">tot <input type="date" placeholder="jjjj-mm-dd" name="boekingsperiode2" min="<?php echo date("Y-m-d") ?>">
 			<br>
 				Aantal personen: <input type="text" name="aantal personen" placeholder="aantal personen">
 			<br>
+				E-mail adres: <input type="text" name="email" placeholder="e-mail adres">
+				<br>
 				<input type="checkbox" id="Caravan" value="Caravan">Caravan
 				<br>
 				<input type="checkbox" id="Tent" value="Tent">Tent
@@ -180,8 +198,5 @@
 			<input type="submit" />
 		</form>
    		</div>
-
-   		
-   		
 	</body>
 </html>
